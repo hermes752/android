e13b6729bbf0422688dd6885bfad0183e74eab67
Unai <hermes752@gmail.com>
2015-03-20 19:26:30 +0100

    fragmentos

--- TodoListFragment/app/src/main/java/com/example/cursomovil/todolistfragment/InputFragment.java
+++ TodoListFragment/app/src/main/java/com/example/cursomovil/todolistfragment/InputFragment.java
@@ -1,6 +1,7 @@
 package com.example.cursomovil.todolistfragment;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,24 +16,53 @@ import android.widget.EditText;
  */
 public class InputFragment extends Fragment {
 
-
-    private Button btnAdd;
-    private EditText todoText;
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View layout=inflater.inflate(R.layout.fragment_input, container, false);
-
-        btnAdd=(Button)layout.findViewById(R.id.buttonAdd);
-        todoText=(EditText)layout.findViewById(R.id.inputText);
+        public interface TODOItemListener{
+            public void addTodo(String todo)
 
 
+        }
 
-        return layout;
-    }
+        private Button btnAdd;
+        private EditText todoText;
+        private Activity target;
 
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            try{
+
+            }
+
+            this.target=activity;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View layout=inflater.inflate(R.layout.fragment_input, container, false);
+
+            btnAdd=(Button)layout.findViewById(R.id.buttonAdd);
+            todoText=(EditText)layout.findViewById(R.id.inputText);
+
+            addEventListener();
+
+            return layout;
+        }
+
+        private void addEventListener(){
+
+            btnAdd.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    String todo=todoText.getText().toString();
+
+
+                }
+            });
+
+
+        }
 
 }
