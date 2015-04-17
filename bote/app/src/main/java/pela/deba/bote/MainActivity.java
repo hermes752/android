5b6a1dcd1d3c4d4ac1f1b9b5c13dd052dacc458f
Unai <hermes752@gmail.com>
2015-04-17 17:45:31 +0200

    prueba

--- bote/app/src/main/java/pela/deba/bote/MainActivity.java
+++ bote/app/src/main/java/pela/deba/bote/MainActivity.java
@@ -1,7 +1,7 @@
-package com.example.cursomovil.bote;
+package pela.deba.bote;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,7 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
